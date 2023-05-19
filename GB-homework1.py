1611434892ca6081294dbbec8d724f3e50a3922d
Polina Kolesnikova <polina@example.com>
2023-05-19 17:34:13 +0400

    adding exercise 8

--- GB-homework1.py
+++ GB-homework1.py
@@ -44,16 +44,16 @@
 #
 # 385916 -> yes
 # 123456 -> no
-ticket = input('Please, enter number of your ticket in format XXXXXX\nand i will tell you is it lucky or not: ')
-while (len(ticket)!=6 or not ticket.isdigit()):
-    print('Wrong format. Reenter!')
-    ticket = input('Please, enter number of your ticket in format XXXXXX: ')
-part1 = sum([int(i) for i in ticket[:3]])
-part2 = sum([int(i) for i in ticket[3:]])
-if part1==part2:
-    print('YES, you are lucky=)')
-else:
-    print('NO, sorry')
+# ticket = input('Please, enter number of your ticket in format XXXXXX\nand i will tell you is it lucky or not: ')
+# while (len(ticket)!=6 or not ticket.isdigit()):
+#     print('Wrong format. Reenter!')
+#     ticket = input('Please, enter number of your ticket in format XXXXXX: ')
+# part1 = sum([int(i) for i in ticket[:3]])
+# part2 = sum([int(i) for i in ticket[3:]])
+# if part1==part2:
+#     print('YES, you are lucky=)')
+# else:
+#     print('NO, sorry')
 #
 # Задача 8: Требуется определить, можно ли от шоколадки размером n × m долек отломить k долек,
 # если разрешается сделать один разлом по прямой между дольками (то есть разломить шоколадку на два прямоугольника).
@@ -63,3 +63,8 @@ else:
 # 3 2 4 -> yes
 # 3 2 1 -> no
 #
+n, m, k = [int(i) for i in input('Please, enter size of chocolate"s tablet(n x m) and quanity of pieces (k)\n in format n m k: ').split()]
+if n*m > k:
+    print('YES' if k%n==0 or k%m==0 else 'NO') # смотрим делится ли количество долек на  какую-нибудь сторону нацело, чтобы можно было бы сделать ТОЛЬКО один разлом
+else:
+    print('NO')
